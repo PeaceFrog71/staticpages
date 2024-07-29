baff88ba22e7c486b699401f4faf9e07c3b64537
PeaceFrog71 <dafrog71@yahoo.com>
2024-07-28 19:38:50 -0600

    Initial commit

---
+++ public/src/main.py
@@ -0,0 +1,6 @@
+from textnode import TextNode
+
+def main():
+	test_node =  TextNode("Some test text", "bold", "https://boot.dev")
+	print(test_node)
+main()
---
+++ public/src/test_textnode.py
@@ -0,0 +1,6 @@
+import unittest
+
+from textnode import TextNode
+
+class TestTextNode(unittest.TestCase):
+	def test_
