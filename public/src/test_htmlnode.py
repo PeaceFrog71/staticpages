8b0660b9f0545c167cfc9ce71baf88e5cd819e6f
PeaceFrog71 <dafrog71@yahoo.com>
2024-08-01 22:01:02 -0600

    Created text to html func in HTMLNode and created tests for same

--- public/src/test_htmlnode.py
+++ public/src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode, LeafNode, ParentNode
+from htmlnode import HTMLNode, LeafNode, ParentNode, text_node_to_html
 from textnode import TextNode
 
 class TestHTMLNode(unittest.TestCase):
@@ -28,7 +28,7 @@ class TestHTMLNode(unittest.TestCase):
     def test_ParentNode(self):
         with self.assertRaises(ValueError) as context: 
             ParentNode(tag="div", props={"class": "bold", "id": "test", "style": "color: red;"})
-        self.assertEqual(str(context.exception), "All parent nodes must have children")
+        self.assertEqual(str(context.exception), "Parent nodes must have children")
         
         node_with_children = ParentNode(
             tag="p",
@@ -39,21 +39,21 @@ class TestHTMLNode(unittest.TestCase):
                 LeafNode(tag="i", value="italic text"),
                 LeafNode(tag=None, value="Normal text"),
             ],
+            props={"class": "bold", "id": "test", "style": "color: red;"}
         )
         print(node_with_children.to_html())
-        self.assertEqual(node_with_children.to_html(), "<p><b>Bold text</b>Normal text<i>italic text</i>Normal text</p>")
-        
-    def test_text_to_html(self):
-        node_boldtxt = TextNode(text_type="b", text="This is some BOLD text")
-        node_italictxt = TextNode("i", "This is some ITALIC text")
-        node_codetxt = TextNode("code", "This is some CODE text")
-        node_linktxt = TextNode("link", "This is a LINK", "www.google.com")
-        node_imgtxt = TextNode("image", "This is an IMAGE", "https://cdn1.vox-cdn.com/uploads/chorus_asset/file/4019352/september-1st-doodle-do-not-translate-5078286822539264-hp.0.gif")
-        
-        node_boldhtml = LeafNode().text_node_to_html(node_boldtxt)
-             
-        
-        
-        self.assertEqual(node_boldhtml, "<b>This is some BOLD text</b>")
-        
-        
+        self.assertEqual(node_with_children.to_html(), "<p class=\"bold\" id=\"test\" style=\"color: red;\"><b>Bold text</b>Normal text<i>italic text</i>Normal text</p>")
+    
+
+    def test_text_html(self):
+        node_boldtxt = TextNode(text_type="bold", text="This is some BOLD text")
+        node_italictxt = TextNode(text_type="italic", text="This is some ITALIC text")
+        node_codetxt = TextNode(text_type="code", text="This is some CODE text")
+        node_linktxt = TextNode(text_type="link", text="This is a LINK", url="www.google.com")
+        node_imgtxt = TextNode(text_type="image", text="This is an IMAGE", url="https://cdn1.vox-cdn.com/uploads/chorus_asset/file/4019352/september-1st-doodle-do-not-translate-5078286822539264-hp.0.gif")
+
+        self.assertEqual(text_node_to_html(node_boldtxt).to_html(), "<b>This is some BOLD text</b>")
+        self.assertEqual(text_node_to_html(node_italictxt).to_html(), "<i>This is some ITALIC text</i>")
+        self.assertEqual(text_node_to_html(node_codetxt).to_html(), "<code>This is some CODE text</code>")
+        self.assertEqual(text_node_to_html(node_linktxt).to_html(), "<a href=\"www.google.com\">This is a LINK</a>")
+        self.assertEqual(text_node_to_html(node_imgtxt).to_html(), "<img src=\"https://cdn1.vox-cdn.com/uploads/chorus_asset/file/4019352/september-1st-doodle-do-not-translate-5078286822539264-hp.0.gif\" alt=\"This is an IMAGE\"></img>")   
