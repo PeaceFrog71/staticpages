432fe5a0e00d7da3f3dbe52926b5248ca2bc8976
PeaceFrog71 <dafrog71@yahoo.com>
2024-11-12 14:52:04 -0700

    Move text type constants to a new text_types.py module for better organization

---
+++ public/src/text_types.py
@@ -0,0 +1,7 @@
+#Text types
+text_type_text = "text"
+text_type_bold = "bold"
+text_type_italic = "italic"
+text_type_code = "code"
+text_type_link = "link"
+text_type_image = "image"
--- public/src/textnode.py
+++ public/src/textnode.py
@@ -1,12 +1,5 @@
 from htmlnode import LeafNode
-
-
-text_type_text = "text"
-text_type_bold = "bold"
-text_type_italic = "italic"
-text_type_code = "code"
-text_type_link = "link"
-text_type_image = "image"
+from text_types import *
 
 class TextNode:
     def __init__(self, text, text_type, url=None):
